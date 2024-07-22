edaeac13814367fbd20d22b5acde9c1d0e80de12
Avleen Kaur <kau16509@sheridancollege.ca>
2024-07-22 17:47:51 -0400

    updated shuffle , initialize, draw card in Card.java, Deck.java

--- SYST17796_ProjectStarterCode1/SYST17796_ProjectStarterCode/src/Card.java
+++ SYST17796_ProjectStarterCode1/SYST17796_ProjectStarterCode/src/Card.java
@@ -1,30 +1,40 @@
 public class Card {
 
-	private String color;
-	private String value;
+    private String color;
+    private String value;
 
-	public String getColor() {
-		return this.color;
-	}
+    public Card(String color, String value) {
+        this.color = color;
+        this.value = value;
+    }
 
-	/**
-	 * 
-	 * @param color
-	 */
-	public void setColor(String color) {
-		this.color = color;
-	}
+    // Getter method for color
+    public String getColor() {
+        return this.color;
+    }
 
-	public String getValue() {
-		return this.value;
-	}
+    /**
+    * 
+    * @param color
+    */
+    public void setColor(String color) {
+        this.color = color;
+    }
 
-	/**
-	 * 
-	 * @param value
-	 */
-	public void setValue(String value) {
-		this.value = value;
-	}
+    public String getValue() {
+        return this.value;
+    }
 
+    /**
+    * 
+    * @param value
+    */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return value + " of " + color;
+    }
 }
--- SYST17796_ProjectStarterCode1/SYST17796_ProjectStarterCode/src/Deck.java
+++ SYST17796_ProjectStarterCode1/SYST17796_ProjectStarterCode/src/Deck.java
@@ -1,20 +1,64 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Deck {
 
-	private List<Card> cards;
+    private List<Card> cards;
 
-	public void shuffle() {
-		// TODO - implement Deck.shuffle
-		throw new UnsupportedOperationException();
-	}
+    public Deck() {
+        this.cards = new ArrayList<>();
+    }
 
-	public Card drawCard() {
-		// TODO - implement Deck.drawCard
-		throw new UnsupportedOperationException();
-	}
+    //Shuffles deck
+    public void shuffle() {
+        Collections.shuffle(cards);
+        System.out.println("Deck has been shuffled.");
+    }
 
-	public void initializeUnoDeck() {
-		// TODO - implement Deck.initializeUnoDeck
-		throw new UnsupportedOperationException();
-	}
+    //Draw a card from top of the deck
+    public Card drawCard() {
+        if (!cards.isEmpty()) {
+            return cards.remove(0);
+        } else {
+            System.out.println("Deck is empty. Can't draw card.");
+            return null;
+        }
+    }
 
+    //Initialize a standard Uno deck
+    public void initializeUnoDeck() {
+        String[] colors = {"Red", "Yellow", "Green", "Blue"};
+        String[] values = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "Skip", "Reverse", "Draw Two"};
+
+        for (String color : colors) {
+            for (String value : values) {
+                cards.add(new Card(color, value));
+                if (!value.equals("0")) {
+                    cards.add(new Card(color, value));
+                }
+            }
+        }
+
+        for (int i = 0; i < 4; i++) {
+            cards.add(new Card("Wild", "Wild"));
+            cards.add(new Card("Wild", "Wild Draw Four"));
+        }
+
+        System.out.println("Initialized Uno deck with " + cards.size() + " cards.");
+    }
+
+    //Testing the Deck class
+    public static void main(String[] args) {
+        Deck deck = new Deck();
+        deck.initializeUnoDeck();
+        deck.shuffle();
+
+        for (int i = 0; i < 5; i++) {
+            Card drawnCard = deck.drawCard();
+            if (drawnCard != null) {
+                System.out.println("Drew card: " + drawnCard);
+            }
+        }
+    }
 }
